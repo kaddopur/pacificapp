771d508946c905e2f86a0ea0942198d31502c203
Kaddo <kaddo.dev@gmail.com>
2014-04-30 02:13:49 +0800

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Pacificapp::Application.config.secret_key_base = '098aa0242003bf4f8b90553b8aa76e796c30c43b665145b02d449997a7619a8ab5afc95c3710b95a402a0b7787f49a1e26e758bc902d64e91b8d85cba532dfe0'
